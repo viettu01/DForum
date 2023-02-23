f2d7c932278d1d5c098c8686e6a85808d909aef8
mannv291201 <88828150+mannv291201@users.noreply.github.com>
2023-02-23 16:03:24 +0700

    demo lan 2

--- app/src/main/java/com/tuplv/dforum/MainActivity.java
+++ app/src/main/java/com/tuplv/dforum/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //Demo git lần 1
+        setContentView(R.layout.activity_main); //Demo git lần 2
     }
 }
