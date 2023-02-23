7155a7b074c4bae3f142a7c92b4c25530fab752e
mannv291201 <88828150+mannv291201@users.noreply.github.com>
2023-02-23 16:29:20 +0700

    add LoginActivity, RegisterActivity and xml

---
+++ app/src/main/java/com/tuplv/dforum/authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.tuplv.dforum.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tuplv.dforum.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
