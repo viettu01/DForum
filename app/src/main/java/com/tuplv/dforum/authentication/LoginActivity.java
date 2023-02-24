98a937bea6579e1148b5e762081e69710bdf5583
mannv291201 <88828150+mannv291201@users.noreply.github.com>
2023-02-24 11:10:39 +0700

    thêm các thư viện, hàm xử lý đăng ký đăng nhập bằng firebase

--- app/src/main/java/com/tuplv/dforum/authentication/LoginActivity.java
+++ app/src/main/java/com/tuplv/dforum/authentication/LoginActivity.java
@@ -1,16 +1,48 @@
 package com.tuplv.dforum.authentication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.tuplv.dforum.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // khởi tạo xác thực firebase
+        mAuth = FirebaseAuth.getInstance();
+    }
+    public void login(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/tuplv/dforum/model/Accounts.java
@@ -0,0 +1,12 @@
+package com.tuplv.dforum.model;
+
+public class Accounts {
+    private String accountId;
+    private String nickName;
+    private String story;
+    private String avatarUrl;
+    private String email;
+    private String password;
+    private String role;
+    private String status;
+}
