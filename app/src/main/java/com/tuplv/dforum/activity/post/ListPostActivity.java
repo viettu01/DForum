e4e0f4b7d7b3d04906565b4fc7065f3cc60922b1
Viet Tu <tupham1120@gmail.com>
2023-04-17 22:12:01 +0700

    Thêm giao diện danh sách bài viết

---
+++ app/src/main/java/com/tuplv/dforum/activity/post/ListPostActivity.java
@@ -0,0 +1,16 @@
+package com.tuplv.dforum.activity.post;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tuplv.dforum.R;
+
+public class ListPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_post);
+    }
+}
