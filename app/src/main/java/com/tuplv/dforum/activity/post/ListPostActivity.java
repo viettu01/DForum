afdc42c2f534775fc62b3ae785fce3ae6b4847bc
Viet Tu <tupham1120@gmail.com>
2023-04-18 15:50:02 +0700

    Sửa giao diện home fragment

--- app/src/main/java/com/tuplv/dforum/activity/post/ListPostActivity.java
+++ app/src/main/java/com/tuplv/dforum/activity/post/ListPostActivity.java
@@ -1,16 +1,189 @@
 package com.tuplv.dforum.activity.post;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.tuplv.dforum.until.Constant.OBJ_POST;
+import static com.tuplv.dforum.until.Constant.STATUS_ENABLE;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.tuplv.dforum.R;
+import com.tuplv.dforum.adapter.PostAdapter;
+import com.tuplv.dforum.interf.OnPostClickListener;
+import com.tuplv.dforum.model.Forum;
+import com.tuplv.dforum.model.Post;
 
-public class ListPostActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class ListPostActivity extends AppCompatActivity implements View.OnClickListener, OnPostClickListener {
+
+    Toolbar tbListPost;
+    TextView tvNameForum, tvTotalPostForum, tvDesForum, tvNoPost;
+    ImageView imvFilterPost;
+    RecyclerView rvListPost;
+    FloatingActionButton fabAddPost;
+
+    PostAdapter postAdapter;
+    List<Post> posts;
+    Forum forum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_post);
+        init();
+        tbListPost.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (forum != null && forum.getForumId() != 0) {
+            posts.clear();
+            getPostByForumId();
+            return;
+        }
+        getAllPost();
+    }
+
+    private void init() {
+        tbListPost = findViewById(R.id.tbListPost);
+        setSupportActionBar(tbListPost);
+        tvNameForum = findViewById(R.id.tvNameForum);
+        tvTotalPostForum = findViewById(R.id.tvTotalPostForum);
+        tvDesForum = findViewById(R.id.tvDesForum);
+        tvNoPost = findViewById(R.id.tvNoPost);
+        imvFilterPost = findViewById(R.id.imvFilterPost);
+        rvListPost = findViewById(R.id.rvListPost);
+        fabAddPost = findViewById(R.id.fabAddPost);
+
+        posts = new ArrayList<>();
+        postAdapter = new PostAdapter(this, R.layout.item_post, posts, this);
+        rvListPost.setAdapter(postAdapter);
+        rvListPost.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
+        forum = (Forum) getIntent().getSerializableExtra("forum");
+        tvNameForum.setText(forum.getName());
+        tvDesForum.setText(forum.getDescription());
+        if (forum.getDescription().equals(""))
+            tvDesForum.setVisibility(View.GONE);
+
+        fabAddPost.setOnClickListener(this);
+        imvFilterPost.setOnClickListener(this);
+    }
+
+    private void getAllPost() {
+        FirebaseDatabase.getInstance().getReference(OBJ_POST).orderByChild("status").equalTo(STATUS_ENABLE)
+                .addValueEventListener(new ValueEventListener() {
+                    @SuppressLint({"NotifyDataSetChanged", "SetTextI18n"})
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        posts.clear();
+                        tvTotalPostForum.setText("(" + snapshot.getChildrenCount() + ")");
+                        if (snapshot.getChildrenCount() > 0) {
+                            tvNoPost.setVisibility(View.GONE);
+                            rvListPost.setVisibility(View.VISIBLE);
+                        } else {
+                            tvNoPost.setVisibility(View.VISIBLE);
+                            rvListPost.setVisibility(View.GONE);
+                        }
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            Post post = dataSnapshot.getValue(Post.class);
+                            posts.add(post);
+                        }
+                        Collections.reverse(posts);
+                        postAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+    }
+
+    private void getPostByForumId() {
+        FirebaseDatabase.getInstance().getReference(OBJ_POST)
+                .orderByChild("forumId").equalTo(Objects.requireNonNull(forum).getForumId())
+                .addValueEventListener(new ValueEventListener() {
+                    @SuppressLint({"SetTextI18n", "NotifyDataSetChanged"})
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dsPost) {
+                        posts.clear();
+                        tvTotalPostForum.setText("(" + dsPost.getChildrenCount() + ")");
+                        if (dsPost.getChildrenCount() > 0) {
+                            tvNoPost.setVisibility(View.GONE);
+                            rvListPost.setVisibility(View.VISIBLE);
+                        } else {
+                            tvNoPost.setVisibility(View.VISIBLE);
+                            rvListPost.setVisibility(View.GONE);
+                        }
+                        for (DataSnapshot dataSnapshot : dsPost.getChildren()) {
+                            Post post = dataSnapshot.getValue(Post.class);
+                            if (Objects.requireNonNull(post).getStatus().equals(STATUS_ENABLE)) {
+                                posts.add(post);
+                            }
+                        }
+                        Collections.reverse(posts);
+                        postAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fabAddPost:
+                if (FirebaseAuth.getInstance().getCurrentUser() != null)
+                    startActivity(new Intent(this, AddPostActivity.class));
+                else
+                    Toast.makeText(this, "Bạn cần đăng nhập để sử dụng chức năng này!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imvFilterPost:
+                Toast.makeText(this, "Lọc", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void goToActivityDetail(Post post) {
+        HashMap<String, Object> updateView = new HashMap<>();
+        updateView.put("view", post.getView() + 1);
+        FirebaseDatabase.getInstance().getReference(OBJ_POST).child(String.valueOf(post.getPostId()))
+                .updateChildren(updateView);
+
+        Intent intent = new Intent(this, DetailPostActivity.class);
+        intent.putExtra("post", post);
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/tuplv/dforum/fragment/HomeFragment.java
+++ app/src/main/java/com/tuplv/dforum/fragment/HomeFragment.java
@@ -1,9 +1,6 @@
 package com.tuplv.dforum.fragment;
 
-import static com.tuplv.dforum.until.Constant.CHIA_SE_KIEN_THUC;
-import static com.tuplv.dforum.until.Constant.HOI_DAP;
-import static com.tuplv.dforum.until.Constant.OBJ_POST;
-import static com.tuplv.dforum.until.Constant.STATUS_ENABLE;
+import static com.tuplv.dforum.until.Constant.OBJ_FORUM;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -13,51 +10,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.tuplv.dforum.R;
-import com.tuplv.dforum.activity.account.LoginActivity;
 import com.tuplv.dforum.activity.forum.ListForumActivity;
-import com.tuplv.dforum.activity.post.AddPostActivity;
-import com.tuplv.dforum.activity.post.DetailPostActivity;
-import com.tuplv.dforum.adapter.PostAdapter;
-import com.tuplv.dforum.interf.OnPostClickListener;
+import com.tuplv.dforum.activity.post.ListPostActivity;
+import com.tuplv.dforum.adapter.ForumAdapter;
+import com.tuplv.dforum.interf.OnForumClickListener;
 import com.tuplv.dforum.model.Forum;
 import com.tuplv.dforum.model.Post;
 
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
-
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
 
-public class HomeFragment extends Fragment implements View.OnClickListener, OnPostClickListener {
+public class HomeFragment extends Fragment implements View.OnClickListener, OnForumClickListener {
 
-    TextView tvNameForum, tvTotalPostForum;
     RelativeLayout rlShowListForum;
-    RecyclerView rvQA, rvShareKnowledge;
-    FloatingActionButton fabAddPost;
-    ImageView imgShowMoreForum;
-    PostAdapter postsQAAdapter, postsShareKnowledgeAdapter;
-    List<Post> postsQA, postsShareKnowledge;
-    Forum forum;
+    RecyclerView rvListForumFeatured;
+    ImageView imvShowMoreForum;
+    ForumAdapter forumAdapter;
+    List<Forum> forums;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -69,89 +50,37 @@ public class HomeFragment extends Fragment implements View.OnClickListener, OnPo
     }
 
     private void init(View view) {
-        rvQA = view.findViewById(R.id.rvQA);
-        rvShareKnowledge = view.findViewById(R.id.rvShareKnowledge);
-        fabAddPost = view.findViewById(R.id.fabAddPost);
-        imgShowMoreForum = view.findViewById(R.id.imgShowMoreForum);
-        tvNameForum = view.findViewById(R.id.tvNameForum);
-        tvTotalPostForum = view.findViewById(R.id.tvTotalPostForum);
         rlShowListForum = view.findViewById(R.id.rlShowListForum);
+        rvListForumFeatured = view.findViewById(R.id.rvListForumFeatured);
+        imvShowMoreForum = view.findViewById(R.id.imvShowMoreForum);
 
-        fabAddPost.setOnClickListener(this);
         rlShowListForum.setOnClickListener(this);
-        imgShowMoreForum.setOnClickListener(this);
+        imvShowMoreForum.setOnClickListener(this);
+
+        forums = new ArrayList<>();
     }
 
-    private void getAllPost() {
-        postsQA = new ArrayList<>();
-        postsQAAdapter = new PostAdapter(getActivity(), R.layout.item_post, postsQA, this);
-        rvQA.setAdapter(postsQAAdapter);
-        rvQA.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
+    private void getListForumFeatured() {
+        forumAdapter = new ForumAdapter(requireActivity(), R.layout.item_forum, forums, this);
+        rvListForumFeatured.setAdapter(forumAdapter);
+        rvListForumFeatured.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
 
-        postsShareKnowledge = new ArrayList<>();
-        postsShareKnowledgeAdapter = new PostAdapter(getActivity(), R.layout.item_post, postsShareKnowledge, this);
-        rvShareKnowledge.setAdapter(postsShareKnowledgeAdapter);
-        rvShareKnowledge.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
-        FirebaseDatabase.getInstance().getReference(OBJ_POST)
-                .orderByChild("status").equalTo(STATUS_ENABLE)
+        FirebaseDatabase.getInstance().getReference(OBJ_FORUM).limitToLast(5)
                 .addValueEventListener(new ValueEventListener() {
                     @SuppressLint({"NotifyDataSetChanged", "SetTextI18n"})
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        postsQA.clear();
-                        postsShareKnowledge.clear();
-                        tvTotalPostForum.setText("(" + snapshot.getChildrenCount() + ")");
+                        forums.clear();
                         for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                            Post post = dataSnapshot.getValue(Post.class);
-                            if (post.getCategoryName().equalsIgnoreCase(HOI_DAP)) {
-                                postsQA.add(post);
-                            }
-                            if (post.getCategoryName().equalsIgnoreCase(CHIA_SE_KIEN_THUC)) {
-                                postsShareKnowledge.add(post);
-                            }
+                            Forum forum = dataSnapshot.getValue(Forum.class);
+                            forums.add(forum);
                         }
-                        Collections.reverse(postsQA);
-                        Collections.reverse(postsShareKnowledge);
-                        postsQAAdapter.notifyDataSetChanged();
-                        postsShareKnowledgeAdapter.notifyDataSetChanged();
+                        Collections.reverse(forums);
+                        forumAdapter.notifyDataSetChanged();
                     }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
-                    }
-                });
-    }
-
-    private void getPostByForumId() {
-        FirebaseDatabase.getInstance().getReference(OBJ_POST)
-                .orderByChild("forumId").equalTo(Objects.requireNonNull(forum).getForumId())
-                .addValueEventListener(new ValueEventListener() {
-                    @SuppressLint({"SetTextI18n", "NotifyDataSetChanged"})
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dsPost) {
-                        postsQA.clear();
-                        postsShareKnowledge.clear();
-                        tvTotalPostForum.setText("(" + dsPost.getChildrenCount() + ")");
-                        for (DataSnapshot dataSnapshot : dsPost.getChildren()) {
-                            Post post = dataSnapshot.getValue(Post.class);
-                            if (Objects.requireNonNull(post).getStatus().equals(STATUS_ENABLE)) {
-                                if (post.getCategoryName().equalsIgnoreCase(HOI_DAP)) {
-                                    postsQA.add(post);
-                                }
-                                if (post.getCategoryName().equalsIgnoreCase(CHIA_SE_KIEN_THUC)) {
-                                    postsShareKnowledge.add(post);
-                                }
-                            }
-                        }
-                        Collections.reverse(postsQA);
-                        Collections.reverse(postsShareKnowledge);
-                        postsQAAdapter.notifyDataSetChanged();
-                        postsShareKnowledgeAdapter.notifyDataSetChanged();
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
                     }
                 });
     }
@@ -160,14 +89,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener, OnPo
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.fabAddPost:
-                if (FirebaseAuth.getInstance().getCurrentUser() != null)
-                    startActivity(new Intent(getActivity(), AddPostActivity.class));
-                else
-                    Toast.makeText(getActivity(), "Bạn cần đăng nhập để sử dụng chức năng này!", Toast.LENGTH_SHORT).show();
-                break;
             case R.id.rlShowListForum:
-            case R.id.imgShowMoreForum:
+            case R.id.imvShowMoreForum:
                 startActivity(new Intent(getActivity(), ListForumActivity.class));
                 break;
         }
@@ -176,44 +99,23 @@ public class HomeFragment extends Fragment implements View.OnClickListener, OnPo
     @Override
     public void onResume() {
         super.onResume();
-        if (forum != null && forum.getForumId() != 0) {
-            postsQA.clear();
-            postsShareKnowledge.clear();
-            getPostByForumId();
-        } else if (forum != null && forum.getForumId() == 0)
-            getAllPost();
-        else
-            getAllPost();
+        getListForumFeatured();
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EventBus.getDefault().register(this);
+    public void goToActivityUpdate(Forum forum) {
+
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        EventBus.getDefault().unregister(this);
-    }
+    public void onDeleteClick(Forum forum) {
 
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onDataEvent(Forum forum) {
-        this.forum = forum;
-        tvNameForum.setText(forum.getName());
     }
 
     @Override
-    public void goToActivityDetail(Post post) {
-        HashMap<String, Object> updateView = new HashMap<>();
-        updateView.put("view", post.getView() + 1);
-        FirebaseDatabase.getInstance().getReference(OBJ_POST).child(String.valueOf(post.getPostId()))
-                .updateChildren(updateView);
-
-        Intent intent = new Intent(getActivity(), DetailPostActivity.class);
-        intent.putExtra("post", post);
-
+    public void goToListPostOfForum(Forum forum) {
+        Intent intent = new Intent(getActivity(), ListPostActivity.class);
+        intent.putExtra("forum", forum);
         startActivity(intent);
     }
 }
